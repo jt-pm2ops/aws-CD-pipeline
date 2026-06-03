c846803d1dacf63b037e7844442a34ef5472a30a
John Tarog <your-github-email@example.com>
2026-06-03 16:35:31 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Practicing aws cd pipeline'));
+app.get('/', (req, res) => res.send('AWS CD Pipeline Created!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
